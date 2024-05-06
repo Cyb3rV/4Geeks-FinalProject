f97428ce817d7a3bdb9a3f5e9130b8ad9bd7a6a5
Cyb3rV <152754161+Cyb3rV@users.noreply.github.com>
2024-05-06 10:55:32 +0000

    Clearing DataContext

--- src/components/DataContext.jsx
+++ src/components/DataContext.jsx
@@ -1,73 +1,6 @@
-// const { useReducer, createContext, useEffect } = require("react");
-
-
-
-// import { createClient } from '@supabase/supabase-js'
-
-// const supabaseUrl = 'https://ijnhfoehgzuzlkriaoak.supabase.co';
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqbmhmb2VoZ3p1emxrcmlhb2FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MTgxNjAsImV4cCI6MjAzMDI5NDE2MH0.cTpLh8D93_WcIl3uo1qwoFbdi9T96lF_fe7BFJnQV6w';
-// // Create a single supabase client for interacting with your database
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-
-// const DataContext = createContext(null);
-
-// async function DataReducer(state, action){
-//     switch(action.type){
-//         case "add":                       
-//             try{          
-//                 await supabase.from(action.payload.table).insert(action.payload.data)
-//                 return { ...state, error: false };
-
-//             } catch(error){
-//                 console.error("Error inserting data:", error.message);
-//                 return { ...state, error: true };
-//             }
-
-
-//         default:
-//             return state;
-//     }
-// }
-
-// const initialState = {
-//     user: "",
-//     password: "",
-//     email: "",
-//     error: false
-// }
-
-
-// export function DataProvider({children}){
-//     const [data, dataActions] = useReducer(DataReducer, initialState);
-
-//     useEffect(() => {
-//         console.log(data.error);
-//       },[]);
-    
-//     //   useEffect(() => {
-//     //     console.log(data.error);
-//     //   },[data]);
-
-//     return(
-//         <DataContext.Provider value={{data, dataActions}}>{children}</DataContext.Provider>
-//     );
-
-// }
-
-// export default DataContext;
-
-
-
-
-
 const { useReducer, createContext, useEffect } = require("react");
-
-
-
 import { createClient } from '@supabase/supabase-js'
-import { act } from 'react';
+
 
 const supabaseUrl = 'https://ijnhfoehgzuzlkriaoak.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqbmhmb2VoZ3p1emxrcmlhb2FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MTgxNjAsImV4cCI6MjAzMDI5NDE2MH0.cTpLh8D93_WcIl3uo1qwoFbdi9T96lF_fe7BFJnQV6w';
@@ -89,27 +22,13 @@ async function DataReducer(state, action){
                 console.error("Error inserting data:", error.message);
                 return state;
             }
-        // case "get_excercises":
-        //     try{
-        //         const { data } = await supabase.from("exercises_list").select("*");
-        //         console.log(data)
-
-        //         // return { ...state, excercises: data, error: false };
-        //     } catch(error){
-        //         console.error("Error getting excercises:", error.message);
-        //         return { ...state, error: true };
-        //     }
-
         case "consult":
             try{
                 const { data } = await supabase.from(action.payload.table).select("*");
-                console.log(data)            
-                
+                console.log(data);
                 return data;
-
-                // return { ...state, excercises: data, error: false };
             } catch(error){
-                console.error("Error getting excercises:", error.message);
+                console.error("Error getting data:", error.message);
                 return state;
             }
 
@@ -123,16 +42,12 @@ async function DataReducer(state, action){
             for (const key in action.payload.data) {
                 const value = action.payload.data[key];            
                 query = query.eq(key, value);
-                
-                //console.log(`Nombre de la propiedad: ${key}, Valor: ${value}`);
             }   
         
-        try{
-                    // const { data } = await supabase.from(action.payload.table).select("*").eq("email", "pepe@gmail.com");
-                    const { data } = await query;
-                    console.log(data)
-    
-                    return data;
+            try{
+                const { data } = await query;
+                console.log(data)
+                return data;
                 } catch(error){
                     console.error("Error getting excercises:", error.message);
                     return state;
@@ -144,49 +59,9 @@ async function DataReducer(state, action){
     }
 }
 
-// const initialState = {
-//     user: "",
-//     password: "",
-//     email: "",
-//     error: false
-// }
-
 
 export function DataProvider({children}){
     const [data, dataActions] = useReducer(DataReducer, null);
-
-    // useEffect(() => {
-    //     console.log(`entro desde context ${data}`);
-    //   },[data]);
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //       const resolvedData = await data;
-    //        console.log(resolvedData);
-    //       console.log("Datos desde el contexto:", JSON.stringify(resolvedData));
-    //     };
-    //     fetchData();
-    //   }, [data]);
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //       const resolvedData = await data;
-    //       if (resolvedData && resolvedData.length > 0) {
-    //         console.log(resolvedData);
-    //         resolvedData.forEach(item => {
-    //           const id = item.id; // Obtiene el valor de la clave "id" de cada objeto
-    //           console.log("ID del usuario:", id);
-    //         });
-    //       }
-    //     };
-    //     fetchData();
-    //   }, [data]);
-      
-      
-    
-    //   useEffect(() => {
-    //     console.log(data.error);
-    //   },[data]);
 
     return(
         <DataContext.Provider value={{data, dataActions}}>{children}</DataContext.Provider>
